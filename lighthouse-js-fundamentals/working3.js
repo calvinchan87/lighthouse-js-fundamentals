ae3cbbaebc27cef5881893f10f8a45c2b1752def
Calvin Chan <calvinchan87@gmail.com>
2021-02-22 01:13:20 +0000

    uploaded 4x working files and 3x javascripting exercises

---
+++ lighthouse-js-fundamentals/working3.js
@@ -0,0 +1,13 @@
+function lastIndexOf(some_array, some_value) {
+  for (let i = some_array.length - 1; i >= -1; i--) {
+    if (some_array[i] === some_value) {
+      return i;
+    }
+  } return -1;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
